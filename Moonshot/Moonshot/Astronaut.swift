dda5dbd75bfa0be777e1d172c9e090406171d061
Divyansh Saini(Div) <divyansh@umich.edu>
2020-06-07 18:17:43 -0400

    Add Moonshot

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Divyansh on 6/7/20.
+//  Copyright © 2020 Div. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let name: String
+    let description: String
+    let id: String
+}
